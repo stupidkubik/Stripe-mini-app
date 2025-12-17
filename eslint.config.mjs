a4a7e4a256a9d8f7b4acfb665e622c401d55e419
Evgenii Rubin <kubikus2009@yandex.ru>
2025-12-17 19:53:28 +0100

    hotfix next16 update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextConfig from "eslint-config-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const config = [
+  ...nextConfig,
   {
     ignores: [
       "node_modules/**",
@@ -19,9 +10,10 @@ const eslintConfig = [
       "build/**",
       "coverage/**",
       "playwright-report/**",
+      "test-results/**",
       "next-env.d.ts",
     ],
   },
 ];
 
-export default eslintConfig;
+export default config;
